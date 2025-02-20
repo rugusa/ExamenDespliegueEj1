25b20fe0fe4c3740305bb720beaebe98942c36f6
rugusa <rugusa@floridauniversitaria.es>
2025-02-20 12:06:58 +0100

    Primer commit

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Factorial PHP</title>
+    <title>Factorial PHP.</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
